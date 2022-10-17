67d21f641ed66c2d5f09ace104def2e3363e3df2
Олег Попов <casporator123@gmail.com>
2022-10-17 23:45:51 +0200

    Resurses: change var = let

--- WorkOutApp/Resourses.swift
+++ WorkOutApp/Resourses.swift
@@ -11,42 +11,42 @@ import UIKit
 
 enum Resourses {
     enum Colors{
-        static var active = UIColor(hexString: "#437BFE")
-        static var inactive = UIColor(hexString: "#929DA5")
-        static var seporator = UIColor(hexString: "#E8ECEF")
-        static var darkGray = UIColor(hexString: "#545C77")
-        static var background = UIColor(hexString: "#F2F2F2")
-        static var secondary = UIColor(hexString: "#F0F3FF")
+        static let active = UIColor(hexString: "#437BFE")
+        static let inactive = UIColor(hexString: "#929DA5")
+        static let seporator = UIColor(hexString: "#E8ECEF")
+        static let darkGray = UIColor(hexString: "#545C77")
+        static let background = UIColor(hexString: "#F2F2F2")
+        static let secondary = UIColor(hexString: "#F0F3FF")
         
     }
     
     enum Strings {
         enum TabBar {
-            static var overview = "Обзор"
-            static var session = "Сессия"
-            static var progress = "Прогресс"
-            static var settings = "Настройки"
+            static let overview = "Обзор"
+            static let session = "Сессия"
+            static let progress = "Прогресс"
+            static let settings = "Настройки"
         }
             enum NavBar {
-            static var navBarLeftButton = "Пауза"
-            static var navBarRightButton = "Финиш"
+            static let navBarLeftButton = "Пауза"
+            static let navBarRightButton = "Финиш"
         }
         enum OverView {
-            static var allWorksoutButton = "Все занятия"
-            static var titleWord = "Сегодня"
+            static let allWorksoutButton = "Все занятия"
+            static let titleWord = "Сегодня"
         }
     }
     enum Images {
         enum TabBar {
-            static var overview = UIImage(named: "House Icon")
-            static var session = UIImage(named:"Clock Icon")
-            static var progress = UIImage(named:"Analytics Bars")
-            static var settings = UIImage(named:"Gear Icon")
+            static let overview = UIImage(named: "House Icon")
+            static let session = UIImage(named:"Clock Icon")
+            static let progress = UIImage(named:"Analytics Bars")
+            static let settings = UIImage(named:"Gear Icon")
         }
         
         enum Commons {
-            static var downArrow = UIImage(named: "Path")
-            static var add = UIImage(named: "Add")
+            static let downArrow = UIImage(named: "Path")
+            static let add = UIImage(named: "Add")
         }
     }
     
